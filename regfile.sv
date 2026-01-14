e5cf49e6426a1452e9c53c8beb0dd8f60fb5fde7
Zane Francis <153263134+zanerf1640@users.noreply.github.com>
2026-01-14 15:15:50 -0600

    Refactor regfile module with clearer comments
    Refactor register file module for clarity and initialization.

--- regfile.sv
+++ regfile.sv
@@ -5,26 +5,24 @@ module regfile (input logic         clk,
 		input logic [31:0]  wd3, 
 		output logic [31:0] rd1, rd2);
    
+   // 32x32-bit Register
    logic [31:0] 		    rf[31:0];
    
-   // three ported register file
+   // Initialize output registers -> 0
    assign rd1 = (ra1 != 0) ? rf[ra1] : 32'b0;
    assign rd2 = (ra2 != 0) ? rf[ra2] : 32'b0;
-   // read two ports combinationally
    always_ff @(posedge clk) begin
       if (reset) begin
          // Reset all registers to 0
          for (int i = 0; i < 32; i++) begin
             rf[i] <= 32'b0;
          end
-      end 
+      end
+      // Write input data to specified register 
       else if (we3 && (wa3 != 0)) begin
          rf[wa3] <= wd3;
       end
    end
-   // write third port on rising edge of clock
-   
-   // register 0 hardwired to 0
    
    
 endmodule // regfile
